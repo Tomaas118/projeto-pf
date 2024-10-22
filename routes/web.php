7ff011e678596db85f88f389825e1bc469c39b16
Tomaas118 <119084466+Tomaas118@users.noreply.github.com>
2024-10-22 18:48:51 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/Login-Medico', function () {
+    return view('loginMedico');
+});
