db951b285bd5b7d9c87444432a0501e789302f67
ggomes45 <guilhermego2008.gg@gmail.com>
2024-11-16 22:37:27 +0000

    display success message

--- resources/views/LoginRegistar/loginMedico.blade.php
+++ resources/views/LoginRegistar/loginMedico.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Login Médico')
 
 @section('content')
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     <form action="{{ route('loginMedico') }}" method="POST">
         @csrf
         <div class="mb-3">
