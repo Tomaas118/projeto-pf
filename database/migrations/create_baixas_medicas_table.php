ab751e068e08e06adc239bc9cba13c6117856f7e
ggomes45 <guilhermego2008.gg@gmail.com>
2024-12-07 21:13:35 +0000

    fix typo in fk id_paciente

--- database/migrations/create_baixas_medicas_table.php
+++ database/migrations/create_baixas_medicas_table.php
@@ -3,14 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+ 
 return new class extends Migration {
     public function up(): void {
 
         Schema::create('baixas_medicas', function (Blueprint $table) {
             $table->id('id');
             $table->foreignId('id_medico')->constrained('medicos', 'id');
-            $table->foreignId('id_paciente')->constrained('paciente', 'id');
+            $table->foreignId('id_paciente')->constrained('pacientes', 'id');
             $table->foreignId('id_unidadeMedica')->constrained('unidade_medica', 'id');
             $table->string('diagnostico', 255);
             $table->date('dataInicio');
